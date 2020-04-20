942147350af8f88dfb30bbeda90d4c6588a5772c
AlekseY506 <45352006+AlekseY506@users.noreply.github.com>
2020-04-20 21:43:09 +0300

    вывод списка пользователей

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+include "library.php";
+//1. Соедининие с БД
+$pdo = new PDO('mysql:host=localhost;dbname=poligon_1;charset=utf8;', 'root', '');
+//include "connectDB.php";
+//2.Выполнить запрос
+$sql = "SELECT * FROM users";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+//3.Получить ассоциативный массив
+$users = $statement->fetchAll(PDO::FETCH_ASSOC);
+//4.Вывести чрез foreach
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Title</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Главная <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">список</a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">email</th>
+                    <th scope="col">name</th>
+                    <th scope="col">regiser</th>
+                    <th scope="col">status</th>
+                    <th scope="col">action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($users as $user): ?>
+                <tr>
+                    <th scope="row"><?=$user['id']?></th>
+                    <td><?=$user['name']?></td>
+                    <td><?=$user['date-register']?></td>
+                    <td><?=$user['email']?></td>
+                    <td><?=$user['status']?></td>
+                    <td>
+                        <a href="#" class="btn btn-danger">блокировать</a>
+                        <a href="#" class="btn btn-success">разблокировать</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
